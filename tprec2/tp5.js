0ef82bd3d7164111b74b177fa527c0329678645c
facuansin <129102558+facuansin@users.noreply.github.com>
2023-11-18 20:19:06 -0300

    Update tp5.js

--- tprec2/tp5.js
+++ tprec2/tp5.js
@@ -1,7 +1,6 @@
 //Facundo Ansin
 //legajo: 93054/5
-//link video: 
-
+//link video: https://www.youtube.com/watch?v=qHMk9uikwt8&ab_channel=FacundoAnsin
 let j;
 function setup() {
   createCanvas(600, 600);
